d93b2e7a73a4776d6adad084de835755281e5704
Muhammadrizo <yusufovmuhammadrizodev@gmail.com>
2022-08-03 15:58:04 +0500

    added command menu

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher, executor, types
 from checkWord import checkWord
 from transliterate import to_cyrillic, to_latin
+from .set_bot_commands import set_default_commands
 
 API_TOKEN='5584667811:AAGdirz2SlyYjLLN7W_gDcgoyIkbB8TJMWk'
 
@@ -13,13 +14,17 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+async def on_startup(dispatcher):
+    # Birlamchi komandalar (/star va /help)
+    await set_default_commands(dispatcher)
+
 @dp.message_handler(commands='start')
 async def send_welcome(message: types.Message):
     await message.answer("Uz Imlo botiga xush kelibsiz!\nBotdan foydalanish uchun so‘z yuboring.")
 
 @dp.message_handler(commands='help')
 async def help_user(message: types.Message):
-    await message.answer("Buyruqlar:\n- /start - Botdan foydalinishni boshlash\n- /help - ushbu xabarni qayta chiqarish\nBotdan foydalanish uchun so‘z yuboring.\nDasturchi: @muhammadrizodev")
+    await message.answer("Buyruqlar:\n- /start - Botdan foydalinishni boshlash\n- /help - ushbu xabarni qayta chiqarish\n\nBotdan foydalanish uchun so‘z yuboring.\nDasturchi: @muhammadrizodev")
 
 @dp.message_handler()
 async def checkImlo(message: types.Message):
@@ -41,4 +46,4 @@ async def checkImlo(message: types.Message):
             await message.answer(response)
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
